5658247df005a11b1672b84a4a37915e4599b3f5
rahul12-99/rahul12-99 <rahulkrrao162@gmail.com>
2023-06-18 10:30:58 +0530

    Added : Code for check employee attendance

--- EmployeeWageBuilder.js
+++ EmployeeWageBuilder.js
@@ -1,1 +1,8 @@
-console.log("Welcome to employee wage computation problem");
+const IS_ABSENT = 0;
+    let employeeCheck = Math.floor(Math.random() * 10) % 2;
+    if(employeeCheck == IS_ABSENT){
+        console.log("Employee is Absent");
+    }
+    else{
+        console.log("Employee is Present");
+    }
