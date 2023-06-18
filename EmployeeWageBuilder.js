1c42dfb1f492a9622007d74c2064f07bbca85b18
rahul12-99/rahul12-99 <rahulkrrao162@gmail.com>
2023-06-18 13:05:00 +0530

    Added : Code for store total wage in array

--- EmployeeWageBuilder.js
+++ EmployeeWageBuilder.js
@@ -3,7 +3,6 @@ const IS_FULL_TIME = 2;
 const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
-
 function getWorkingHours(employeeCheck) {
     switch(employeeCheck){
         case IS_PART_TIME:
@@ -14,15 +13,22 @@ function getWorkingHours(employeeCheck) {
             return 0;
     }   
 }
+
+function calculateDailyWage(employeeHrs){
+    return employeeHrs * WAGE_PER_HOUR;
+}
 const MAX_HRS_IN_MONTH = 160;
 const NUM_OF_WORKING_DAY = 20;
 let totalEmployeeHrs = 0;
 let totalWorkingDay = 0;
+let employeeDailyWageArr = new Array();
 while(totalEmployeeHrs <= MAX_HRS_IN_MONTH &&
          totalWorkingDay < NUM_OF_WORKING_DAY){
             totalWorkingDay++;
             let employeeCheck = Math.floor(Math.random() * 10) % 3;
-            totalEmployeeHrs += getWorkingHours(employeeCheck);
-         }
-let employeeWage = totalEmployeeHrs * WAGE_PER_HOUR;
-console.log("Total Days: " + totalWorkingDay + " " + "Total Hours: " + totalEmployeeHrs +" " + "Total Employee Wage: " + employeeWage);
+            let employeeHrs = getWorkingHours(employeeCheck);
+            totalEmployeeHrs += employeeHrs;
+            employeeDailyWageArr.push(calculateDailyWage(employeeHrs));
+         } 
+let employeeWage = calculateDailyWage(totalEmployeeHrs);
+console.log("Total Days: " + totalWorkingDay + " " + "Total Hours: " + totalEmployeeHrs +" " + "Employee Wage: " + employeeWage);
