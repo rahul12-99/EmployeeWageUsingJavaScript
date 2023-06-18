03754a5147d6855cf74fbc34e12bce7b1aceb56e
rahul12-99/rahul12-99 <rahulkrrao162@gmail.com>
2023-06-18 10:36:41 +0530

    Added : code for ability to calculate full time or part time wage

--- EmployeeWageBuilder.js
+++ EmployeeWageBuilder.js
@@ -1,8 +1,20 @@
-const IS_ABSENT = 0;
-    let employeeCheck = Math.floor(Math.random() * 10) % 2;
-    if(employeeCheck == IS_ABSENT){
-        console.log("Employee is Absent");
-    }
-    else{
-        console.log("Employee is Present");
-    }
+const IS_PART_TIME = 1;
+const IS_FULL_TIME = 2;
+const PART_TIME_HOURS = 4;
+const FULL_TIME_HOURS = 8;
+const WAGE_PER_HOUR = 20;
+
+let employeeHrs = 0;
+employeeCheck = Math.floor(Math.random() * 10) % 3;
+switch(employeeCheck){
+    case IS_PART_TIME:
+        employeeHrs = PART_TIME_HOURS;
+        break;
+    case IS_FULL_TIME:
+        employeeHrs = FULL_TIME_HOURS;
+        break;
+    default:
+        employeeHrs = 0;
+}
+let employeeWage = employeeHrs * WAGE_PER_HOUR;
+console.log("Employee Wage: " + employeeWage);
